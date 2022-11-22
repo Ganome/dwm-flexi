3e6f4a9807a45f67e5d82c04d46fe5ecb99fb479
Ganome <ganome@gmail.com>
2022-11-21 21:54:31 -0700

    Initial Commit of DWM-FlexiPatch https://github.com/bakkeby/dwm-flexipatch

---
+++ patch/include.h
@@ -0,0 +1,23 @@
+/* Bar functionality */
+#include "bar_indicators.h"
+#include "bar_tagicons.h"
+#include "bar.h"
+
+#include "bar_ltsymbol.h"
+#include "bar_status.h"
+#include "bar_tags.h"
+#include "bar_wintitle.h"
+#include "bar_systray.h"
+
+/* Other patches */
+#include "attachx.h"
+#include "cool_autostart.h"
+#include "cyclelayouts.h"
+#include "movestack.h"
+#include "pertag.h"
+#include "restartsig.h"
+/* Layouts */
+#include "layout_grid.h"
+#include "layout_monocle.h"
+#include "layout_tile.h"
+
